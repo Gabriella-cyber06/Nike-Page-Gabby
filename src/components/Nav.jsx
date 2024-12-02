c894452be4099d7cd56dcab32a5f29e823febf60
gabriella-cyber06 <oluwaferanmiakinpelumi@gmail.com>
2024-12-02 12:16:46 +0100

    updated site

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import headerLogo from '../assets/images/header-Logo.svg'
+import headerLogo from '../assets/images/header-logo.svg'
 import { navLinks } from '../constants'
 import { hamburger, close } from '../assets/icons'
 import { useState } from 'react'
